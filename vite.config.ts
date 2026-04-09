3fee6dccec4ec3462b86d861be056f19e4fc3722
It's Me RIIYAD🤡💋 <riadalhasan202@gmail.com>
2026-04-09 18:44:49 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,27 @@
 import { defineConfig } from 'vite';
-import obfuscator from 'vite-plugin-obfuscator';
+import { viteObfuscator as obfuscator } from 'vite-plugin-obfuscator';
 
 export default defineConfig({
   plugins: [
     obfuscator({
-      options: {
-        compact: true,
-        controlFlowFlattening: true,
-        controlFlowFlatteningThreshold: 1,
-        deadCodeInjection: true,
-        deadCodeInjectionThreshold: 1,
-        debugProtection: true,
-        debugProtectionInterval: 1000,
-        disableConsoleOutput: true,
-        identifierNamesGenerator: 'hexadecimal',
-        log: false,
-        numbersToExpressions: true,
-        renameGlobals: true,
-        selfDefending: true,
-        splitStrings: true,
-        splitStringsChunkLength: 2,
-        stringArray: true,
-        stringArrayEncoding: ['rc4', 'base64'],
-        stringArrayThreshold: 1,
-        transformObjectKeys: true,
-        unicodeEscapeSequence: true
-      },
+      compact: true,
+      controlFlowFlattening: true,
+      controlFlowFlatteningThreshold: 1,
+      deadCodeInjection: true,
+      deadCodeInjectionThreshold: 1,
+      debugProtection: true,
+      debugProtectionInterval: 1000,
+      disableConsoleOutput: true,
+      identifierNamesGenerator: 'hexadecimal',
+      numbersToExpressions: true,
+      renameGlobals: true,
+      selfDefending: true,
+      splitStrings: true,
+      splitStringsChunkLength: 2,
+      stringArray: true,
+      stringArrayEncoding: ['rc4'],
+      stringArrayThreshold: 1,
+      unicodeEscapeSequence: true
     }),
   ],
   build: {
